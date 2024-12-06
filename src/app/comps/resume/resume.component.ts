d115f73fca73a48bc06673195269e7dc6ef78822
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-12-06 07:32:06 +0500

    a

---
+++ src/app/comps/resume/resume.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resume',
+  standalone: true,
+  templateUrl: './resume.component.html',
+  styleUrls: ['./resume.component.scss'],
+})
+export class ResumeComponent {}
